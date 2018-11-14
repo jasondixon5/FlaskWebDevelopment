6f54f18c9f1e29f9f1f51429f1ae2d1170edbc45
Jason Dixon <jasondixon@Jasons-MBP.lan>
2018-11-14 13:53:23 -0800

    added command line tools via flask-script.

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 from flask import Flask
+# book import line appears to be deprecated
+# from flask.ext.script import Manager
+from flask_script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route("/")
 def index():
@@ -10,6 +15,7 @@ def user(name):
     return "<h1>Hello %s!</h1>" % name
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    manager.run()
 
 
