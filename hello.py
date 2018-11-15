91ad357ddddb53acaeda457e91ea584548ff2884
Jason Dixon <jasondixon@NicholassiPhone.localdomain>
2018-11-15 11:49:23 -0800

    added relationships to db model

--- hello.py
+++ hello.py
@@ -69,6 +69,7 @@ class Role(db.Model):
     __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    users = db.relationship("User", backref="role")
 
     def __repr__(self):
         return "<Role {}>".format(self.name)
@@ -78,6 +79,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
 
     def __repr__(self):
         return "<User {}>".format(self.username)
