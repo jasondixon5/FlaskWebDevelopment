4fd867bea9378ca74401821d843fe3e06abbac59
Jason Dixon <jasondixon@NicholassiPhone.localdomain>
2018-11-15 10:37:38 -0800

    added flash message; corrected error in hello.py with import statement

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 # book import line appears to be deprecated
 # from flask.ext.script import Manager
 from flask_script import Manager
@@ -25,6 +25,9 @@ def index():
     form = NameForm()
 
     if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
         session["name"] = form.name.data
         return redirect(url_for("index"))
     
